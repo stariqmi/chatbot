2d2f90a658941ab4d6c90c30072cee8e72f4857d
Salman <salman@ubuntu.ubuntu-domain>
2013-06-11 21:20:42 -0400

    simple validation for chatbot creation completed

--- client/helpers/effects.js
+++ client/helpers/effects.js
@@ -1,7 +1,13 @@
 Meteor.startup(function() {
+
+	// Event handler for the create button
 	$(".create_button").click(function() {
+
+		// Extracting the value of the input fields 
 		var name = $(".name_input").val();
 		var pwd = $(".pwd_input").val();
+
+		// Validation
 		if( name == "" ) {
 			$(".error p").text("Please enter a name for your chatbot");
 			$(".error").fadeIn();
@@ -12,11 +18,27 @@ Meteor.startup(function() {
 		}
 
 		else {
-			$(".error").css("background-color", "green");
 			var bot_name = name.replace(" ", "_");
-			var message  = "Your chatbot's url is chatbot.meteorapp.com/" + bot_name;
-			$(".error p").text(message);
-			$(".error").fadeIn();
+				
+			var existing_bots = Chatbots.find({name: bot_name}).fetch();
+			console.log(existing_bots.length);
+
+			if(existing_bots.length != 0) {
+				var message  = "A chatbot by this name already exists";
+				$(".error p").text(message);
+				$(".error").fadeIn();
+			}
+			else {
+				Chatbots.insert({
+					name: bot_name,
+					password: pwd
+				})
+				$(".error").css("background-color", "green");
+				var message  = "The url for your chatbot is chatbot.meteorapp.com/" + bot_name;
+				$(".error p").text(message);
+				$(".error").fadeIn();	
+			}
+			
 		}
 	})
 })
